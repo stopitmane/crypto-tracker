31b4155be365c4a9901f7826dd6cb3005dd3cf3d
Ajayi John <ajayijohn19999@gmail.com>
2025-04-15 16:55:52 +0100

    Fix: coinChart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CoinChart from './CoinChart'; // Adjust the path if necessary
+
 
 const App = () => {
   const [coins, setCoins] = useState([]);
